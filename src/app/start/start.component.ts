e4abb33d5496db7566c688b579f076af4e4c8bf0
Sven B <SvenBrunner@web.de>
2023-11-28 22:35:14 +0100

    header und footer

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
