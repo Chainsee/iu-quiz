9fb776fa265b6e6557af39c387522794dd78971d
Sven B <SvenBrunner@web.de>
2023-11-29 21:55:07 +0100

    game style angepasst

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
 })
 export class GameComponent {
   fragen: frage[] = [
-    {frage: "Frage1", antwort: ["Antwort1", "Antwort2", "Antwort3", "Antwort4"], korrekteAntwort: 1},
+    {frage: "Frage1", antwort: ["Antwort1", "Antwort2", "Antwort3", "Antwort4"], korrekteAntwort: 1, kategorie: "Test"},
     {frage: "Frage2", antwort: ["Antwort1", "Antwort2", "Antwort3", "Antwort4"], korrekteAntwort: 2},
     {frage: "Frage3", antwort: ["Antwort1", "Antwort2", "Antwort3", "Antwort4"], korrekteAntwort: 3},
     {frage: "Frage4", antwort: ["Antwort1", "Antwort2", "Antwort3", "Antwort4"], korrekteAntwort: 4},
