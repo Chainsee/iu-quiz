dba064ebd20d1a22d96bcad2ffcab58238929138
Sven B <SvenBrunner@web.de>
2023-12-04 17:55:31 +0100

    color-scheme für richtige und falsche Fragen angepasst

---
+++ dist/iu-quiz/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZV5M66Q3.mjs";import"./chunk-2AFYYHEZ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,20 +9,51 @@ import { CommonModule } from '@angular/common';
   styleUrl: './game.component.scss',
 })
 export class GameComponent {
+  isRightAnswer: boolean[] = [
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    true,
+  ];
 
-  isRightAnswer: boolean = false;
   notAnswered: boolean = true;
 
   fragen: frage[] = [
-    {frage: "Frage1", antwort: ["Antwort1", "Antwort2", "Antwort3", "Antwort4"], korrekteAntwort: 1, kategorie: "Test"},
-    {frage: "Frage2", antwort: ["Antwort1", "Antwort2", "Antwort3", "Antwort4"], korrekteAntwort: 2},
-    {frage: "Frage3", antwort: ["Antwort1", "Antwort2", "Antwort3", "Antwort4"], korrekteAntwort: 3},
-    {frage: "Frage4", antwort: ["Antwort1", "Antwort2", "Antwort3", "Antwort4"], korrekteAntwort: 4},
-  ]
+    {
+      frage: 'Frage1',
+      antwort: ['Antwort1', 'Antwort2', 'Antwort3', 'Antwort4'],
+      korrekteAntwort: 1,
+      kategorie: 'Test',
+    },
+    {
+      frage: 'Frage2',
+      antwort: ['Antwort1', 'Antwort2', 'Antwort3', 'Antwort4'],
+      korrekteAntwort: 2,
+    },
+    {
+      frage: 'Frage3',
+      antwort: ['Antwort1', 'Antwort2', 'Antwort3', 'Antwort4'],
+      korrekteAntwort: 3,
+    },
+    {
+      frage: 'Frage4',
+      antwort: ['Antwort1', 'Antwort2', 'Antwort3', 'Antwort4'],
+      korrekteAntwort: 4,
+    },
+  ];
+
+  getWrongAnswer(array: number) {
+    return !this.isRightAnswer[array];
+  }
 }
 type frage = {
-  frage: string,
-  antwort: string[],
-  korrekteAntwort: number,
-  kategorie?: string,
-}
+  frage: string;
+  antwort: string[];
+  korrekteAntwort: number;
+  kategorie?: string;
+};
